d55dd0bddce63a5440fb41d5cb72f70df7f0a7f2
yougandar <ynaik@sysgain.com>
2017-10-27 00:55:27 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -22,6 +22,7 @@ sudo apt-get install -y jenkins
 sleep 10
 #for installing hxselect#
 sudo apt install html-xml-utils
+sudo apt-get update
 sleep 10
 # for opening the ports#
 sudo apt install -y firewalld
