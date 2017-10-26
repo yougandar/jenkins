9bae9a3574b4f8191f114ac6ef67f321d5eb8778
yougandar <ynaik@sysgain.com>
2017-10-26 23:53:42 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#!/bin/bash
 #Date - 26102017
 #Developer - Sysgain
 
+#setting the variables#
 DATE=`date +%Y%m%d%T`
 LOG=/tmp/jenkins_deploy.log.$DATE
 srcdir="/usr/share/jenkins"
@@ -9,14 +11,35 @@ jenkinsdir="/var/lib/jenkins"
 user="admin"
 passwd=`cat /var/lib/jenkins/secrets/initialAdminPassword`
 url="localhost:8080"
-
-
+#installing jenkins#
+sudo apt-get update
+sudo apt-get install -y default-jre 
+sudo apt-get install -y default-jdk sleep 10
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install -y jenkins 
+sleep 10
+#for installing hxselect#
+sudo apt install html-xml-utils
+# for opening the ports#
+apt install -y firewalld
+firewall-cmd --list-ports
+firewall-cmd --zone=public --add-port=8080/tcp --permanant
+firewall-cmd --zone=public --add-port=8080/tcp --permanent
+firewall-cmd --reload
+service jenkins restart 
+sleep 20
+#setting the permissions
+sudo chmod 777 /var/lib/jenkins/secrets
+sudo chmod 777 /var/lib/jenkins/secrets/initialAdminPassword
+#getting the configuration file
 #Download the Required Jenkins Files
 echo "---Download the Required Jenkins Files---" >> $LOG
-sudo wget -P $srcdir https://raw.githubusercontent.com/yougandar/test/master/job-configfile.xml >> $LOG
+sudo wget -P /usr/share/jenkins https://raw.githubusercontent.com/yougandar/test/master/job-configfile.xml >> $LOG
 #Configuring Jenkins
 echo "---Configuring Jenkins---"
-sudo wget -P $srcdir http://$url/jnlpJars/jenkins-cli.jar
+sudo wget -P /usr/share/jenkins http://localhost:8080/jnlpJars/jenkins-cli.jar
 java -jar $srcdir/jenkins-cli.jar -s http://$url who-am-i --username $user --password $passwd
 api=`curl --silent --basic http://$user:$passwd@$url/user/admin/configure | hxselect '#apiToken' | sed 's/.*value="\([^"]*\)".*/\1\n/g'`
 CRUMB=`curl 'http://'$user':'$api'@'$url'/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'`
@@ -28,4 +51,5 @@ sleep 30 && java -jar $srcdir/jenkins-cli.jar -s  http://$url restart --username
 sleep 30 && java -jar $srcdir/jenkins-cli.jar -s  http://$url restart --username $user --password $passwd && sleep 30
 
 sudo curl -X POST "http://$user:$api@$url/createItem?name=GameofLifeJob" --data-binary "@$srcdir/job-configfile.xml" -H "$CRUMB" -H "Content-Type: text/xml"
+
 
