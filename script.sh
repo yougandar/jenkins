e4a67e313ee0e87d83ac188d595e30f0a3443638
yougandar <ynaik@sysgain.com>
2017-10-26 17:56:28 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,7 +12,8 @@ url="localhost:8080"
 
 sudo apt-get update
 sudo apt-get install -y default-jre 
-sudo apt-get install -y default-jdk sleep 10
+sudo apt-get install -y default-jdk sleep 20
+sudo apt-get install html-xml-utils sleep 10
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
