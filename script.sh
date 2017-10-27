7672ea72ad213c7148f825b406cf2b1c83ad4e1b
yougandar <ynaik@sysgain.com>
2017-10-27 12:01:24 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -17,7 +17,8 @@ sudo apt-get install -y default-jdk sleep 10
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
-sudo apt-get install -y jenkins 
+sudo apt-get install -y jenkins
+sudo service jenkins restart
 sleep 10
 # for opening the ports#
 #sudo apt install -y firewalld
