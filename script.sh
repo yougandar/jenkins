69f9f9f315dc0235867a8c1dffc60c8989c9308a
yougandar <ynaik@sysgain.com>
2017-10-27 12:19:36 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,15 +10,6 @@ jenkinsdir="/var/lib/jenkins"
 user="admin"
 passwd=`cat /var/lib/jenkins/secrets/initialAdminPassword`
 url="localhost:8080"
-#installing jenkins#
-sudo apt-get update
-sudo apt-get install -y default-jre 
-sudo apt-get install -y default-jdk sleep 10
-wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get install -y jenkins
-sleep 20
 # for opening the ports#
 #sudo apt install -y firewalld
 #sudo firewall-cmd --zone=public --add-port=8080/tcp
