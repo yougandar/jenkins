24ee30d88d56af41bfc1c34fb0f8e68e0b235aa0
yougandar <ynaik@sysgain.com>
2017-10-27 11:56:40 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -21,12 +21,12 @@ sudo apt-get update
 sudo apt-get install -y jenkins 
 sleep 10
 # for opening the ports#
-sudo apt install -y firewalld
-sudo firewall-cmd --zone=public --add-port=8080/tcp
-sudo firewall-cmd --zone=public --add-port=8080/tcp 
-sudo firewall-cmd --reload
-sudo service jenkins restart 
-sleep 20
+#sudo apt install -y firewalld
+#sudo firewall-cmd --zone=public --add-port=8080/tcp
+#sudo firewall-cmd --zone=public --add-port=8080/tcp 
+#sudo firewall-cmd --reload
+#sudo service jenkins restart 
+#sleep 20
 #for installing hxselect#
 sudo apt install html-xml-utils
 sudo apt-get update
