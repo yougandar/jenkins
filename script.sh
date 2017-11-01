9903edba79df8c4affa51c999e5dfea1c393caca
yougandar <ynaik@sysgain.com>
2017-11-01 13:11:22 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#Date - 26102017
-#Developer - Sysgain
-#setting the variables#
 DATE=`date +%Y%m%d%T`
 LOG=/tmp/jenkins_deploy.log.$DATE
 srcdir="/usr/share/jenkins"
@@ -14,27 +11,23 @@ sudo apt-get update
 sudo apt install -y html-xml-utils
 sudo sleep 20
 #setting the permissions
-sudo chmod +x /var/lib/jenkins/secrets
-sudo chmod +x /var/lib/jenkins/secrets/initialAdminPassword
+sudo chmod 777 /var/lib/jenkins/secrets
+sudo chmod 777 /var/lib/jenkins/secrets/initialAdminPassword
 #Download the Required Jenkins Files
 echo "---Download the Required Jenkins Files---" >> $LOG
 sudo wget -P /usr/share/jenkins https://raw.githubusercontent.com/yougandar/test/master/job-configfile.xml >> $LOG
-sudo sleep 20
 #Configuring Jenkins
 echo "---Configuring Jenkins---"
-#sudo wget http://$url/jnlpJars/jenkins-cli.jar
-#sudo cp jenkins-cli.jar $srcdir
-#sudo wget -P $srcdir http://$url/jnlpJars/jenkins-cli.jar
 sudo wget -P /usr/share/jenkins http://localhost:8080/jnlpJars/jenkins-cli.jar
 sudo java -jar $srcdir/jenkins-cli.jar -s http://$url who-am-i --username $user --password $passwd
 sudo api=`curl --silent --basic http://$user:$passwd@$url/user/admin/configure | hxselect '#apiToken' | sed 's/.*value="\([^"]*\)".*/\1\n/g'`
 sudo CRUMB=`curl 'http://'$user':'$api'@'$url'/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'`
-sudo echo $api
-sudo echo $CRUMB
-#systemctl restart jenkins
-#sleep 30 && java -jar $srcdir/jenkins-cli.jar -s  http://$url restart --username $user --password $passwd
+echo $api
+echo $CRUMB
 #creating jenkins user
-sudo sleep 30 && sudo java -jar $srcdir/jenkins-cli.jar -s  http://$url restart --username $user --password $passwd && sudo sleep 30  
+sudo sleep 30 
+sudo java -jar $srcdir/jenkins-cli.jar -s  http://$url restart --username $user --password $passwd
+sudo sleep 30  
 sudo curl -X POST "http://$user:$api@$url/createItem?name=GameofLifeJob" --data-binary "@$srcdir/job-configfile.xml" -H "$CRUMB" -H "Content-Type: text/xml"
 
 
