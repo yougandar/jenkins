c43cdf961b39ced112f16c006e9fd06723925bf2
yougandar <ynaik@sysgain.com>
2017-11-02 11:32:13 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
+#Date - 26102017
+#Developer - Sysgain
+#setting the variables#
 DATE=`date +%Y%m%d%T`
 LOG=/tmp/jenkins_deploy.log.$DATE
 srcdir="/usr/share/jenkins"
 jenkinsdir="/var/lib/jenkins"
 user="admin"
 passwd=`cat /var/lib/jenkins/secrets/initialAdminPassword`
-url="127.0.0.1:8080"
+url="localhost:8080"
 #for installing hxselect#
 sudo apt-get update
-sudo apt install -y html-xml-utils >> $LOG
+sudo apt install -y html-xml-utils
 sleep 20
 #setting the permissions
-sudo chmod 777 /var/lib/jenkins/secrets >> $LOG
-sudo chmod 777 /var/lib/jenkins/secrets/initialAdminPassword >> $LOG
+sudo chmod 777 /var/lib/jenkins/secrets
+sudo chmod 777 /var/lib/jenkins/secrets/initialAdminPassword
 #Download the Required Jenkins Files
 echo "---Download the Required Jenkins Files---" >> $LOG
 wget -P /usr/share/jenkins https://raw.githubusercontent.com/yougandar/test/master/job-configfile.xml >> $LOG
+sleep 20
 #Configuring Jenkins
-echo "---Configuring Jenkins---" >> $LOG
-cd /home/ubuntu/
-echo "`pwd`" >> $LOG
-wget http://${url}/jnlpJars/jenkins-cli.jar >> $LOG
-sudo cp /home/ubuntu/jenkins-cli.jar /usr/share/jenkins/ >> $LOG
-#wget -P /usr/share/jenkins http://localhost:8080/jnlpJars/jenkins-cli.jar
-java -jar $srcdir/jenkins-cli.jar -s http://${url} who-am-i --username $user --password $passwd >> $LOG
-api=`curl --silent --basic http://$user:$passwd@${url}/user/admin/configure | hxselect '#apiToken' | sed 's/.*value="\([^"]*\)".*/\1\n/g'` >> $LOG
-CRUMB=`curl 'http://'$user':'$api'@'${url}'/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'` >> $LOG
-echo $api >> $LOG
-echo $CRUMB >> $LOG
-#creating jenkins user
+echo "---Configuring Jenkins---"
+#sudo wget http://$url/jnlpJars/jenkins-cli.jar
+#sudo cp jenkins-cli.jar $srcdir
+#sudo wget -P $srcdir http://$url/jnlpJars/jenkins-cli.jar
+wget -P /usr/share/jenkins http://localhost:8080/jnlpJars/jenkins-cli.jar
+java -jar $srcdir/jenkins-cli.jar -s http://$url who-am-i --username $user --password $passwd
+api=`curl --silent --basic http://$user:$passwd@$url/user/admin/configure | hxselect '#apiToken' | sed 's/.*value="\([^"]*\)".*/\1\n/g'`
+CRUMB=`curl 'http://'$user':'$api'@'$url'/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'`
+echo $api
+echo $CRUMB
 sleep 30 
-java -jar $srcdir/jenkins-cli.jar -s  http://${url} restart --username $user --password $passwd
+java -jar $srcdir/jenkins-cli.jar -s  http://$url restart --username $user --password $passwd 
 sleep 30  
-curl -X POST "http://$user:$api@${url}/createItem?name=GameofLifeJob" --data-binary "@$srcdir/job-configfile.xml" -H "$CRUMB" -H "Content-Type: text/xml"
+curl -X POST "http://$user:$api@$url/createItem?name=GameofLifeJob" --data-binary "@$srcdir/job-configfile.xml" -H "$CRUMB" -H "Content-Type: text/xml"
